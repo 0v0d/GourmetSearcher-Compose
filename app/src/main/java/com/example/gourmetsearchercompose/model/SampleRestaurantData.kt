149c0718feec82156d9933cba177ceeced020026
0v0d <energyremtu@gmail.com>
2024-07-13 06:51:32 +0900

    モックデータを一つのファイルに定義した

---
+++ app/src/main/java/com/example/gourmetsearchercompose/model/SampleRestaurantData.kt
@@ -0,0 +1,63 @@
+package com.example.gourmetsearchercompose.model
+
+import com.example.gourmetsearchercompose.model.api.BudgetData
+import com.example.gourmetsearchercompose.model.api.GenreData
+import com.example.gourmetsearchercompose.model.api.LargeAreaData
+import com.example.gourmetsearchercompose.model.api.PCData
+import com.example.gourmetsearchercompose.model.api.PhotoData
+import com.example.gourmetsearchercompose.model.api.Shops
+import com.example.gourmetsearchercompose.model.api.SmallAreaData
+import com.example.gourmetsearchercompose.model.api.Urls
+import com.example.gourmetsearchercompose.model.domain.ShopsDomain
+import com.example.gourmetsearchercompose.model.domain.toDomain
+import kotlinx.collections.immutable.toImmutableList
+
+object SampleRestaurantData {
+    val mockRestaurantData = Shops(
+        "モックレストランデータレストラン1",
+        "大阪府大阪市中央区難波１ - 7",
+        "なんば",
+        LargeAreaData("大阪"),
+        SmallAreaData("大阪難波"),
+        GenreData("居酒屋"),
+        BudgetData("2001～3000 円"),
+        "なんば駅徒歩1分",
+        Urls("http://www.testdata.jp/strJ003785192/?vos=nhppalsa000016"),
+        PhotoData(PCData(l = "http://test.jpg")),
+        "月～金",
+        "なし"
+    ).toDomain()
+
+    val mockRestaurantList = listOf(
+        Shops(
+            "モックレストランデータレストラン1",
+            "大阪府大阪市中央区難波１ - 7",
+            "なんば",
+            LargeAreaData("大阪"),
+            SmallAreaData("大阪難波"),
+            GenreData("居酒屋"),
+            BudgetData("2001～3000 円"),
+            "なんば駅徒歩1分",
+            Urls("http://www.testdata.jp/strJ003785192/?vos=nhppalsa000016"),
+            PhotoData(PCData(l = "http://test.jpg")),
+            "月～金",
+            "なし"
+        ).toDomain(),
+        Shops(
+            "モックレストランデータレストラン2",
+            "大阪府大阪市中央区難波１ - 7",
+            "なんば",
+            LargeAreaData("大阪"),
+            SmallAreaData("大阪難波"),
+            GenreData("居酒屋"),
+            BudgetData("2001～3000 円"),
+            "なんば駅徒歩1分",
+            Urls("http://www.testdata.jp/strJ003785192/?vos=nhppalsa000016"),
+            PhotoData(PCData(l = "http://test.jpg")),
+            "月～金",
+            "なし"
+        ).toDomain()
+    ).toImmutableList()
+
+    val mockEmptyRestaurantList = emptyList<ShopsDomain>().toImmutableList()
+}
--- app/src/main/java/com/example/gourmetsearchercompose/ui/screen/preview/SearchLocationContentPreview.kt
+++ app/src/main/java/com/example/gourmetsearchercompose/ui/screen/preview/SearchLocationContentPreview.kt
@@ -1,13 +1,11 @@
 package com.example.gourmetsearchercompose.ui.screen.preview
 
-import android.Manifest
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gourmetsearchercompose.state.LocationSearchState
 import com.example.gourmetsearchercompose.theme.AppTheme
 import com.example.gourmetsearchercompose.ui.screen.seachlocation.SearchLocationContent
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.PermissionStatus
 
 /** 位置情報検索画面プレビュー */
@@ -31,6 +29,21 @@ private fun SearchLocationContentLoadingPreview() {
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 private fun SearchLocationContentErrorPreview() {
+    SearchLocationContent(
+        onClick = {},
+        searchState = LocationSearchState.Error,
+        locationPermissionsState = FakePermissionState(PermissionStatus.Granted),
+        onRetry = {},
+        onOpenSettings = {}
+    )
+}
+
+/** 位置情報検索画面拒否エラープレビュー */
+@Suppress("UnusedPrivateMember")
+@OptIn(ExperimentalPermissionsApi::class)
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun SearchLocationContentDeniedErrorPreview() {
     SearchLocationContent(
         onClick = {},
         searchState = LocationSearchState.Error,
@@ -72,12 +85,29 @@ private fun SearchLocationContentLoadingDarkPreview() {
     }
 }
 
-/** 位置情報検索画面エラーダークモードプレビュー */
+/** 位置情報検索画面エラープレビュー */
 @Suppress("UnusedPrivateMember")
 @OptIn(ExperimentalPermissionsApi::class)
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
 private fun SearchLocationContentErrorDarkPreview() {
+    AppTheme(darkTheme = true) {
+        SearchLocationContent(
+            onClick = {},
+            searchState = LocationSearchState.Error,
+            locationPermissionsState = FakePermissionState(PermissionStatus.Granted),
+            onRetry = {},
+            onOpenSettings = {}
+        )
+    }
+}
+
+/** 位置情報検索画面拒否エラーダークモードプレビュー */
+@Suppress("UnusedPrivateMember")
+@OptIn(ExperimentalPermissionsApi::class)
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+@Composable
+private fun SearchLocationContentDeniedErrorDarkPreview() {
     AppTheme(darkTheme = true) {
         SearchLocationContent(
             onClick = {},
@@ -111,20 +141,5 @@ private fun SearchLocationContentRationalRequiredDarkPreview() {
             onRetry = {},
             onOpenSettings = {}
         )
-    }
-}
-
-/**
- * パーミッションステータスを返すダミークラス
- * @param initialStatus パーミッションステータス
- * @return パーミッションステータス
- */
-@OptIn(ExperimentalPermissionsApi::class)
-private class FakePermissionState(initialStatus: PermissionStatus) : PermissionState {
-    override val permission: String = Manifest.permission.ACCESS_COARSE_LOCATION
-    override var status: PermissionStatus = initialStatus
-
-    @Suppress("EmptyFunctionBlock")
-    override fun launchPermissionRequest() {
     }
 }
