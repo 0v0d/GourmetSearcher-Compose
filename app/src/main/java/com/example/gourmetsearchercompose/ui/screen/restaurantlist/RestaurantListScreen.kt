056d815e8238e64e8580f843cc04da32ee17f0ac
0v0d <legolesia@gmail.com>
2024-11-19 21:00:46 +0900

    不要なimportの削除

--- app/src/main/java/com/example/gourmetsearchercompose/ui/screen/restaurantlist/RestaurantListScreen.kt
+++ app/src/main/java/com/example/gourmetsearchercompose/ui/screen/restaurantlist/RestaurantListScreen.kt
@@ -4,15 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.gourmetsearchercompose.model.data.SearchTerms
 import com.example.gourmetsearchercompose.model.domain.ShopsDomain
-import com.example.gourmetsearchercompose.mock.MockRestaurantData
-import com.example.gourmetsearchercompose.state.SearchState
-import com.example.gourmetsearchercompose.ui.screen.preview.RestaurantListContentWrapper
-import com.example.gourmetsearchercompose.ui.screen.preview.component.PreviewWrapper
 import com.example.gourmetsearchercompose.ui.screen.restaurantlist.component.RestaurantListContent
 import com.example.gourmetsearchercompose.viewmodel.RestaurantListViewModel
 
