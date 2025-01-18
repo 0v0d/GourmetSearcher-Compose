8d4ea7b225cb4b79cf654120adea318e62b9d6b5
0v0d <legolesia@gmail.com>
2025-01-18 22:48:28 +0900

    Serializableの継承が不必要なため削除

--- core/src/main/java/com/example/core/model/data/CurrentLocation.kt
+++ core/src/main/java/com/example/core/model/data/CurrentLocation.kt
@@ -1,17 +1,17 @@
 package com.example.core.model.data
 
-import java.io.Serializable
+import kotlinx.serialization.Serializable
 
 /**
  * 現在地の緯度経度を保持するデータクラス
  * @param latitude 緯度
  * @param longitude 経度
  */
-@kotlinx.serialization.Serializable
+@Serializable
 data class CurrentLocation(
     val latitude: Double,
     val longitude: Double,
-) : Serializable {
+) {
     private companion object {
         @Suppress("ConstPropertyName", "ObjectPropertyNaming")
         private const val serialVersionUID = 1L
